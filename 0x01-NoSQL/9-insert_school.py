29b1e4759916190208c38c796de34e0cc175d463
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-14 17:17:06 +0100

    make script executable when imported

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,7 +3,6 @@
 import pymongo
 
 
-if __name__ == '__main__':
-    def insert_school(mongo_collection, **kwargs):
-        """Returns the new _id"""
-        return mongo_collection.insert_one(kwargs).inserted_id
+def insert_school(mongo_collection, **kwargs):
+    """Returns the new _id"""
+    return mongo_collection.insert_one(kwargs).inserted_id
