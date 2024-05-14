06c588868b58242a11c00b551755e3d35f9596e3
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-14 08:57:48 +0100

    list all documents

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Defines a function that lists all documents in collection"""
+import pymongo
 
 
-if __name__ == '__main__':
-    def list_all(mongo_collection):
-        """Returns a list of document"""
-        return [] if not mongo_collection else list(mongo_collection.find())
+# if __name__ == '__main__':
+def list_all(mongo_collection):
+    """Returns a list of document"""
+    return [] if not mongo_collection else list(mongo_collection.find())
