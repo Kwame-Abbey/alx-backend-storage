fcd9be245dd0e1041533c8a3824bf04c4121556d
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-14 17:06:25 +0100

    Insert document using pymongo

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Inserts a new document in a collection"""
+import pymongo
+
+
+if __name__ == '__main__':
+    def insert_school(mongo_collection, **kwargs):
+        """Returns the new _id"""
+        return mongo_collection.insert_one(kwargs).inserted_id
