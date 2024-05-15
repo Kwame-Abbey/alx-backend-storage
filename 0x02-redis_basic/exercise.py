919642edb4dc7bd5fb76627be9f224c44fb6be59
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-15 19:18:51 +0100

    Callable and Optional

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Write strings to redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -17,3 +17,19 @@ class Cache:
         rand_key = str(uuid.uuid4())
         self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self,
+            key: str, fn: Optional[Callable] = None) -> str:
+        """takes a key string argument and an optional
+        Callable argument named fn
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, data: str) -> str:
+        """returns str value of decoded byte """
+        return data.decode('utf-8', 'strict')
+
+    def get_int(self, data: str) -> int:
+        """returns int value of decoded byte """
+        return int(data)
