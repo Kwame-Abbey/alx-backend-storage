09aa09606e69c862e42896170c6c7b5e4c8f5424
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-15 19:30:25 +0100

    a decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,35 +1,76 @@
 #!/usr/bin/env python3
-"""Write strings to redis"""
+"""
+Basics of Redis
+"""
+from typing import Union, Callable, Optional
+from functools import wraps
 import redis
 import uuid
-from typing import Union, Callable, Optional
+
+
+def call_history(method: Callable) -> Callable:
+    """Stores the history of inputs and outputs for a particular function"""
+    method_key = method.__qualname__
+    inputs, outputs = method_key + ':inputs', method_key + ':outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.rpush(inputs, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(result))
+        return result
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """Creates and returns function that increments the count"""
+    method_key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method_key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Displays history of calls"""
+    method_key = method.__qualname__
+    inputs, outputs = method_key + ':inputs', method_key + ':outputs'
+    redis = method.__self__._redis
+    method_count = redis.get(method_key).decode('utf-8')
+    print(f'{method_key} was called {method_count} times:')
+    IOTuple = zip(redis.lrange(inputs, 0, -1), redis.lrange(outputs, 0, -1))
+    for inp, outp in list(IOTuple):
+        attr, data = inp.decode("utf-8"), outp.decode("utf-8")
+        print(f'{method_key}(*{attr}) -> {data}')
 
 
 class Cache:
-    """Creates a class"""
-    def __init__(self) -> None:
-        """Initializes once an instance is created"""
+    """Cache class to handle redis operations."""
+    def __init__(self):
+        """stores an instance of the Redis client."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Returns the key"""
-        rand_key = str(uuid.uuid4())
-        self._redis.set(rand_key, data)
-        return rand_key
+        """Takes and stores a data argument and returns a string."""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
 
     def get(self,
             key: str, fn: Optional[Callable] = None) -> str:
-        """takes a key string argument and an optional
-        Callable argument named fn
-        """
+        """Takes a key string argument and an optional"""
         data = self._redis.get(key)
         return fn(data) if fn is not None else data
 
     def get_str(self, data: str) -> str:
-        """returns str value of decoded byte """
+        """Returns str value of decoded byte """
         return data.decode('utf-8', 'strict')
 
     def get_int(self, data: str) -> int:
-        """returns int value of decoded byte """
+        """Returns int value of decoded byte """
         return int(data)
